92a1253f5c3ba465e6ca80400e8dc6ca879100b5
Damien Clemenceau <damien.clemenceau@outlook.com>
2018-01-27 01:59:46 +0100

    Create basic camera & player scripts

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+    public GameObject follow;
+
+	void Start () {
+        if(follow == null)
+        {
+            follow = GameObject.FindGameObjectWithTag("Player");
+        }
+	}
+	
+	void Update () {
+		if(follow != null)
+        {
+            transform.position = new Vector3(follow.transform.position.x, follow.transform.position.y, -10);
+        }
+	}
+}
---
+++ Assets/Scripts/Entity/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Vector2 velocity;
+    private Vector2 directionnalInput;
+    private float velocitySmoothing;
+    private float gravity = -5;
+
+    public float speed;
+
+    void Start () {
+	}
+	
+	void Update () {
+        float targetVelocityX = Input.GetAxisRaw("Horizontal") * speed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocitySmoothing, 0.01f);
+     //   velocity.y += gravity * Time.deltaTime;
+
+        transform.position += (Vector3)(velocity * Time.deltaTime);
+    }
+}
