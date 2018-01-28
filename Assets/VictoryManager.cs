4633076efc5804c3291de1a67b5bdbe8e2d5bc8c
Damien Clemenceau <damien.clemenceau@outlook.com>
2018-01-28 13:18:15 +0100

    Script victory screen

---
+++ Assets/VictoryManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryManager : MonoBehaviour {
+    
+	void Update () {
+		if(Input.GetButton("Interact") && Input.GetButton("Pause"))
+        {
+            SceneManager.LoadScene(0);
+        }
+	}
+}
